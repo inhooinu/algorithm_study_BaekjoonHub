ab25136446f0837c3998ae766a21991350ddb146
inhooinu <59385391+inhooinu@users.noreply.github.com>
2024-10-15 19:29:16 +0900

    [Silver III] Title: N과 M (2), Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -0,0 +1,11 @@
+import sys
+from itertools import combinations
+
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+
+for i in combinations([x for x in range(1, n+1)], m):
+    for j in i:
+        print(j, end=' ')
+    print()
