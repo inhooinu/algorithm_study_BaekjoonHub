fe47eac5b796f0a075f15a39d7332adfe804387f
inhooinu <59385391+inhooinu@users.noreply.github.com>
2026-02-02 13:09:53 +0900

    [Silver III] Title: N과 M (5), Time: 220 ms, Memory: 30828 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.java
@@ -0,0 +1,58 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+// N개의 자연수 중에서 M개를 고른 수열
+public class Main {
+	
+	static int N;
+	static int M;
+	static int[] nums;
+	static boolean[] visited;
+	static int[] result;
+	static StringBuilder sb = new StringBuilder();
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		nums = new int[N];
+		visited = new boolean[N];
+		result = new int[M];
+		
+		st = new StringTokenizer(br.readLine());
+		for (int i=0; i<N; i++) {
+			nums[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(nums);
+//		System.out.println(Arrays.toString(nums));
+		
+		select(0);
+		System.out.println(sb);
+	}
+	
+	static void select(int idx) {
+		if (idx==M) {
+			for (int i=0; i<M; i++) {
+//				System.out.print(result[i]+" ");
+				sb.append(result[i]).append(" ");
+			}
+//			System.out.println();
+			sb.append("\n");
+			return;
+		}
+		
+		for (int i=0; i<N; i++) {
+			if (visited[i]) continue;
+			visited[i] = true;
+			result[idx] = nums[i];
+			select(idx+1);
+			result[idx] = 0;
+			visited[i]=false;
+		}
+	}
+}
