3ce9e3c30cdec1a3477e7c7e1f1ee2cb92f95aa6
inhooinu <59385391+inhooinu@users.noreply.github.com>
2026-02-11 23:33:45 +0900

    [Silver III] Title: 달팽이, Time: 4508 ms, Memory: 194584 KB -BaekjoonHub

--- 백준/Silver/1913. 달팽이/달팽이.java
+++ 백준/Silver/1913. 달팽이/달팽이.java
@@ -1,53 +1,52 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
+
     static int N;
-    static int target;
-    static int[][] result;
-    static int[] dr = {-1,0,1,0};
-    static int[] dc = {0,1,0,-1};
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         N = sc.nextInt();
-        target = sc.nextInt();
-        result = new int[N][N];
-
-        int nr = N/2;  // 중앙에서 시작
-        int nc = N/2;  // 중앙에서 시작
-        int num = 1;
-        result[nr][nc] = num++;  // 1부터
-
-        int dir = 0;  // 이동 방향
-        int moveNum = 1;  // 이동할 횟수
-        while (true) {
-            for (int i=0; i<moveNum; i++) {
-                nr = nr + dr[dir];
-                nc = nc + dc[dir];
-
-                result[nr][nc]=num++;
-                if (nr==0 && nc==0) break;
+        int target = sc.nextInt();
+        int[][] arr = new int[N][N];
+        int num = N*N;
+        int r = 0;
+        int c = 0;
+        int dir = 0;
+        int[] dr = {1,0,-1,0};
+        int[] dc = {0,1,0,-1};
+        while (num>0) {
+            // 아래 -> 오른 -> 위 -> 왼
+            arr[r][c] = num--;
+            int nr = r + dr[dir];
+            int nc = c + dc[dir];
+            if (!inRange(nr,nc) || arr[nr][nc]!=0) {
+                dir = (dir+1)%4;  // 방향 전환
+                r += dr[dir];
+                c += dc[dir];
+            } else {
+                r = nr;
+                c = nc;
             }
-            if (num>N*N) break;
-            dir = (dir+1)%4;  // 방향 전환
-            if (dir==0 || dir==2) moveNum++;  // 이동할 횟수 증가
         }
 
-        // 배열 출력
         int targetR = -1;
         int targetC = -1;
         for (int i=0; i<N; i++) {
             for (int j=0; j<N; j++) {
-                System.out.print(result[i][j]+" ");
-                if (result[i][j]==target) {
+                System.out.print(arr[i][j]+" ");
+                if (arr[i][j]==target) {
                     targetR = i+1;
                     targetC = j+1;
                 }
             }
             System.out.println();
         }
-        // 좌표 출력
-        System.out.println(targetR + " " + targetC);
+        System.out.println(targetR+" "+targetC);
     }
 
+    private static boolean inRange(int r, int c) {
+        return r>=0 && r<N && c>=0 && c<N;
+    }
 }
