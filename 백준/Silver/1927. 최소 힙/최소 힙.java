33103e2f66240d04c310cc03486ff221f8175b76
inhooinu <59385391+inhooinu@users.noreply.github.com>
2026-02-15 15:45:40 +0900

    [Silver II] Title: 최소 힙, Time: 660 ms, Memory: 101076 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -0,0 +1,24 @@
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        StringBuilder sb = new StringBuilder();
+        for (int i=0; i<N; i++) {
+            int x = sc.nextInt();
+            if (x==0) {
+                if (pq.isEmpty()) {
+                    sb.append(0).append("\n");
+                } else {
+                    sb.append(pq.poll()).append("\n");
+                }
+            } else {
+                pq.offer(x);
+            }
+        }
+        System.out.println(sb);
+    }
+}
