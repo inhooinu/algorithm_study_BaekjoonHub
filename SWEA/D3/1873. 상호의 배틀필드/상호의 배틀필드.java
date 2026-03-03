42dca8732f4903b21623ab8d23cf048c6eb46c37
inhooinu <59385391+inhooinu@users.noreply.github.com>
2026-03-03 13:37:38 +0900

    [D3] Title: 상호의 배틀필드, Time: 89 ms, Memory: 25,344 KB -BaekjoonHub

--- SWEA/D3/1873. 상호의 배틀필드/상호의 배틀필드.java
+++ SWEA/D3/1873. 상호의 배틀필드/상호의 배틀필드.java
@@ -1,106 +1,176 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Solution {
-
-	static int T; // 테스트 케이스 수
-	static int H, W; // H:높이, W:너비
-	static int N; 
-	static char[][] map;
-	static int cr, cc;
 	
+	static int T;
+	static int H, W;
+	static char[][] map;
+	static int N;
+	static int curR, curC, dir;  // 0: 위, 1: 아래, 2: 왼쪽, 3: 오른쪽
+	static int[] dr = {-1,1,0,0};
+	static int[] dc = {0,0,-1,1};
+
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
+		StringTokenizer st;
 		T = Integer.parseInt(br.readLine());
-		StringBuilder sb = new StringBuilder();
-		for (int test_case = 1; test_case <= T; test_case++) {
-			sb.append("#").append(test_case).append(" ");
-			StringTokenizer st = new StringTokenizer(br.readLine());
+		for (int t=1; t<=T; t++) {
+			
+			st = new StringTokenizer(br.readLine());
 			H = Integer.parseInt(st.nextToken());
 			W = Integer.parseInt(st.nextToken());
 			map = new char[H][W];
-			for (int i = 0; i < H; i++) {
-				String s = br.readLine();
-				for (int j = 0; j < W; j++) {
-					map[i][j] = s.charAt(j);
-					
-					if(map[i][j] == '^' || map[i][j] == 'v' || map[i][j] == '<' || map[i][j] == '>') {
-						cr = i;
-						cc = j;
+			
+			for (int i=0; i<H; i++) {
+				String line = br.readLine();
+				for (int j=0; j<W; j++) {
+					map[i][j] = line.charAt(j);
+					if (map[i][j]=='^') {
+						curR = i;
+						curC = j;
+						dir = 0;
+					} else if (map[i][j]=='v') {
+						curR = i;
+						curC = j;
+						dir = 1;
+					} else if (map[i][j]=='<') {
+						curR = i;
+						curC = j;
+						dir = 2;
+					} else if (map[i][j]=='>') {
+						curR = i;
+						curC = j;
+						dir = 3;
 					}
 				}
 			}
+//			printMap();
+			
 			N = Integer.parseInt(br.readLine());
-			String cmdString = br.readLine();
-			for (int i = 0; i < N; i++) {
-				char cmd = cmdString.charAt(i);
-				if(cmd == 'U') {
-					map[cr][cc] = '^';
-					move(-1, 0);
-				} else if(cmd == 'D') {
-					map[cr][cc] = 'v';
-					move(1, 0);
-				} else if(cmd == 'L') {
-					map[cr][cc] = '<';
-					move(0, -1);
-				} else if(cmd == 'R') {
-					map[cr][cc] = '>';
-					move(0, 1);
-				} else if(cmd == 'S') {
+			String line = br.readLine();
+			for (int i=0; i<N; i++) {
+				char c = line.charAt(i);
+				if (c=='U') {
+					up();
+				} else if (c=='D') {
+					down();
+				} else if (c=='L') {
+					left();
+				} else if (c=='R') {
+					right();
+				} else if (c=='S') {
 					shoot();
 				}
+//				printMap();
 			}
 			
-			for (int i = 0; i < H; i++) {
-				for (int j = 0; j < W; j++) {
-					sb.append(map[i][j]);
-				}
-				sb.append('\n');
+			System.out.print("#"+t+" ");
+			printMap();
+		}
+	}
 
+	private static void shoot() {
+		
+		int nr = curR;
+		int nc = curC;
+		while (true) {
+			nr = nr+dr[dir];
+			nc = nc+dc[dir];
+			if (!inRange(nr,nc)) break;
+			if (map[nr][nc]=='*') {
+				map[nr][nc] = '.';
+				break;
 			}
-//			sb.append('\n');
-		}
-		System.out.println(sb);
-	}
-	
-	static void move(int dr, int dc) {
-		int nr = cr + dr;
-		int nc = cc + dc;
-		if(!check(nr, nc)) return;
-		if(map[nr][nc] != '.') return;
-		map[nr][nc] = map[cr][cc];
-		map[cr][cc] = '.';
-		cr = nr;
-		cc = nc;
-	}
-	
-	static void shoot() {
-		int ar = cr;
-		int ac = cc;
-		int dr = 0, dc = 0;
-		if(map[cr][cc] == '^') {
-			dr = -1;
-		}else if(map[cr][cc] == 'v') {
-			dr = 1;
-		}else if(map[cr][cc] == '<') {
-			dc = -1;
-		}else if(map[cr][cc] == '>') {
-			dc = 1;
-		}
-		while(true) {
-			ar += dr;
-			ac += dc;
-			if(!check(ar, ac)) break;
-			if(map[ar][ac] == '#') break;
-			if(map[ar][ac] == '*') {
-				map[ar][ac] = '.';
+			if (map[nr][nc]=='#') {
 				break;
 			}
 		}
+		
 	}
-	static boolean check(int r, int c) {
-		return r < H && c < W && r >= 0 && c >= 0;
+
+	private static void right() {
+		dir = 3;
+		map[curR][curC] = '>';
+		int nr = curR;
+		int nc = curC+1;
+		
+		if (!inRange(nr, nc)) return;
+		if (map[nr][nc]=='.') {
+			map[curR][curC] = '.';
+			curR = nr;
+			curC = nc;
+			map[curR][curC] = '>';
+		}
+		
+	}
+
+	private static void left() {
+		dir = 2;
+		map[curR][curC] = '<';
+		int nr = curR;
+		int nc = curC-1;
+		
+		if (!inRange(nr, nc)) return;
+		if (map[nr][nc]=='.') {
+			map[curR][curC] = '.';
+			curR = nr;
+			curC = nc;
+			map[curR][curC] = '<';
+		}
+		
+	}
+
+	private static void down() {
+		dir = 1;
+		map[curR][curC] = 'v';
+		int nr = curR+1;
+		int nc = curC;
+		
+		if (!inRange(nr, nc)) return;
+		if (map[nr][nc]=='.') {
+			map[curR][curC] = '.';
+			curR = nr;
+			curC = nc;
+			map[curR][curC] = 'v';
+		}
+	}
+
+	private static void up() {
+		dir = 0;
+		map[curR][curC] = '^';
+		int nr = curR-1;
+		int nc = curC;
+		
+		if (!inRange(nr, nc)) return;
+		if (map[nr][nc]=='.') {
+			map[curR][curC] = '.';
+			curR = nr;
+			curC = nc;
+			map[curR][curC] = '^';
+		}
+	}
+
+	private static boolean inRange(int r, int c) {
+		return r>=0 && r<H && c>=0 && c<W;
+	}
+
+	private static void printMap() {
+		StringBuilder sb = new StringBuilder();
+		for (int i=0; i<H; i++) {
+			for (int j=0; j<W; j++) {
+//				System.out.print(map[i][j]);
+				sb.append(map[i][j]);
+			}
+//			System.out.println();
+			sb.append("\n");
+		}
+//		System.out.println();
+//		sb.append("\n");
+		System.out.print(sb);
 	}
 
 }
