b3bf468d8bf0f9a2be374a7d4dee5bc4ceaa3d16
inhooinu <59385391+inhooinu@users.noreply.github.com>
2026-03-04 15:38:28 +0900

    [Gold IV] Title: 최단경로, Time: 992 ms, Memory: 127456 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -1,60 +1,75 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
 
 public class Main {
-	static int V, E, K;
-	static List<int[]>[] graph;
+	
+	static int V, E;  // 정점의 개수, 간선의 개수
+	static int K;  // 시작정점의 번호
+	static ArrayList<Edge>[] graph;
+	static class Edge implements Comparable<Edge> {
+		int v, w;
+		Edge(int v, int w) {
+			this.v = v;
+			this.w = w;
+		}
+		@Override
+		public int compareTo(Edge o) {
+			return Integer.compare(this.w, o.w);
+		}		
+	}
 	static int[] dist;
-
-	public static void main(String[] args) throws Exception {
+	static boolean[] visited;
+	static int maxVal = Integer.MAX_VALUE/10;
+	
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		V = Integer.parseInt(st.nextToken());
 		E = Integer.parseInt(st.nextToken());
 		K = Integer.parseInt(br.readLine());
-		graph = new ArrayList[V + 1];
-		for (int i = 0; i <= V; i++) {
+		graph = new ArrayList[V+1];
+		for (int i=1; i<=V; i++) {
 			graph[i] = new ArrayList<>();
 		}
-		for (int i = 0; i < E; i++) {
+		for (int i=0; i<E; i++) {
 			st = new StringTokenizer(br.readLine());
 			int u = Integer.parseInt(st.nextToken());
 			int v = Integer.parseInt(st.nextToken());
 			int w = Integer.parseInt(st.nextToken());
-			graph[u].add(new int[] {v, w});
+			graph[u].add(new Edge(v, w));  // Edge(다음 정점, 간선 가중치)
+		}
+		dist = new int[V+1];
+		for (int i=1; i<=V; i++) {
+			dist[i] = maxVal;
+		}
+		dist[K] = 0;
+		visited = new boolean[V+1];
+		
+		PriorityQueue<Edge> pq = new PriorityQueue<>();  // Edge(정점, 시작점에서의 누적거리)
+		pq.offer(new Edge(K,0));  // 시작점
+		while (!pq.isEmpty()) {
+			Edge cur = pq.poll();
+			if (visited[cur.v]) continue;  // 이미 최단거리 확정
+			visited[cur.v] = true;
+			for (Edge next: graph[cur.v]) {
+				if (dist[next.v] > dist[cur.v] + next.w) {
+					dist[next.v] = dist[cur.v] + next.w;
+					pq.offer(new Edge(next.v, dist[next.v]));
+//					System.out.println(Arrays.toString(dist));
+				}
+			}
 		}
 		
-		dijkstra(K);
-		StringBuilder sb = new StringBuilder();
-		for (int i = 1; i <= V; i++) {
-			sb.append(dist[i] == Integer.MAX_VALUE ? "INF" : dist[i]).append('\n');
-		}
-		System.out.println(sb);
-	}
-	
-	static void dijkstra(int start) {
-		dist = new int[V+1];
-		Arrays.fill(dist, Integer.MAX_VALUE);
-		PriorityQueue<int[]> pq = new PriorityQueue<>(new Comparator<int[]>() {
-			@Override
-			public int compare(int[] o1, int[] o2) {
-				return Integer.compare(o1[1], o2[1]);
-			}
-			
-		});
-		dist[start] = 0;
-		pq.offer(new int[] {start, 0});
-		while(!pq.isEmpty()) {
-			int[] cur = pq.poll();
-			
-			if(cur[1] != dist[cur[0]]) continue;
-			
-			for (int[] next : graph[cur[0]]) {
-				int nextDist = cur[1] + next[1];
-				if(nextDist < dist[next[0]]) {
-					dist[next[0]] = nextDist;
-					pq.offer(new int[] {next[0], nextDist});
-				}
+		for (int i=1; i<=V; i++) {
+			if (dist[i]==maxVal) {
+				System.out.println("INF");
+			} else {
+				System.out.println(dist[i]);
 			}
 		}
 	}
