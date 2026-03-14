8b062297eb9c219ee7ad11e41063efbc23226b6c
inhooinu <59385391+inhooinu@users.noreply.github.com>
2026-03-14 21:19:22 +0900

    [Silver III] Title: N과 M (1), Time: 1604 ms, Memory: 67404 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,45 +1,40 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
-    static int N;
-    static int M;
-    static int[] nums;
+
+    static int N, M;
     static boolean[] visited;
     static int[] result;
 
+    // 1부터 N까지의 자연수 중에서 중복 없이 M개를 고른 수열
+    // 순열
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         N = sc.nextInt();
         M = sc.nextInt();
-        nums = new int[N];
-        visited = new boolean[N];
+        visited = new boolean[N+1];
         result = new int[M];
 
-        // 1~N 자연수 배열 만들기
-        for (int i=0; i<N; i++) {
-            nums[i] = i+1;
-        }
-
-        // 중복 없이 M개 고르기
-        select(0);  // 0번째 위치부터 숫자 선택하기
+        select(0);
     }
 
-    public static void select(int idx) {
-        if (idx==M) {  // 만약 M개를 모두 선택했으면
-//            System.out.println(Arrays.toString(result));
-            for (int i=0; i<M; i++) {
-                System.out.print(result[i] + " ");
+    private static void select(int cnt) {
+        if (cnt==M) {
+            for (int i=0; i<result.length; i++) {
+                System.out.print(result[i]+" ");
             }
             System.out.println();
             return;
         }
 
-        for (int i=0; i<N; i++) {  // 숫자 후보 탐색
-            if (visited[i]) continue;  // 이미 선택을 했으면 중복 선택을 할 수 없으므로 패스
-            visited[i] = true;  // 선택한 숫자 표시
-            result[idx] = nums[i];  // 선택한 숫자 결과 배열에 넣기
-            select(idx+1);  // 다음 위치의 숫자 선택하기
-            result[idx] = 0;
+        for (int i=1; i<=N; i++) {
+            if (visited[i]) continue;
+
+            visited[i] = true;
+            result[cnt] = i;
+            select(cnt+1);
+            result[cnt] = 0;
             visited[i] = false;
         }
     }
