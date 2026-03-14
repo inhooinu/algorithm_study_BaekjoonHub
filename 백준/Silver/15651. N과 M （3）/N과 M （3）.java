9c7e34bb2fe878c46e2b1ea2d8d9547a8c0905e8
inhooinu <59385391+inhooinu@users.noreply.github.com>
2026-03-14 23:08:49 +0900

    [Silver III] Title: N과 M (3), Time: 624 ms, Memory: 315656 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.java
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -1,43 +1,37 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
-    static int N;
-    static int M;
-    static int[] nums;
+
+    static int N, M;
     static int[] result;
-    static StringBuilder sb = new StringBuilder();
+    static StringBuilder sb;
 
+    // 1부터 N까지 자연수 중에서 M개를 고른 수열
+    // 중복허용
     public static void main(String[] args) {
-        // 1부터 N까지 자연수 중에서 M개를 고른 수열
-        // 중복 허용
-
         Scanner sc = new Scanner(System.in);
         N = sc.nextInt();
         M = sc.nextInt();
-        nums = new int[N];
         result = new int[M];
+        sb = new StringBuilder();
 
-        // 1부터 N까지 숫자 배열 만들기
-        for (int i=0; i<N; i++) {
-            nums[i] = i+1;
-        }
-
-        select(0);  // 0번째 위치부터 숫자 채우기
-        System.out.print(sb);
+        select(0);
+        System.out.println(sb);
     }
 
-    public static void select(int idx) {
-        if (idx==M) {
-            for (int i=0; i<M; i++) {
-                sb.append(result[i]).append(" ");
+    private static void select(int cnt) {
+        if (cnt==M) {
+            for (int i=0; i<result.length; i++) {
+                sb.append(result[i]+" ");
             }
             sb.append("\n");
             return;
         }
 
-        for (int i=0; i<N; i++) {
-            result[idx]=nums[i];
-            select(idx+1);
+        for (int i=1; i<=N; i++) {
+            result[cnt]=i;
+            select(cnt+1);
         }
     }
 }
