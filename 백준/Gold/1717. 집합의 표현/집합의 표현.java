a1be1e0b176180081a2797720570ba37287f2499
inhooinu <59385391+inhooinu@users.noreply.github.com>
2026-03-15 18:43:17 +0900

    [Gold V] Title: 집합의 표현, Time: 1348 ms, Memory: 57188 KB -BaekjoonHub

--- 백준/Gold/1717. 집합의 표현/집합의 표현.java
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.java
@@ -4,61 +4,61 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	
-	static int n, m;
-	static int[] parents;
-	static int[] rank;
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		n = Integer.parseInt(st.nextToken());
-		m = Integer.parseInt(st.nextToken());
-		parents = new int[n+1];
-		rank = new int[n+1];
-		makeSet();
-		
-		for (int i=0; i<m; i++) {
-			st = new StringTokenizer(br.readLine());
-			int command = Integer.parseInt(st.nextToken());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			if (command==0) {  // 합집합 연산
-				union(a,b);
-			} else if (command==1) {  // 같은 집합에 포함되어 있는지 확인하는 연산
-				if (find(a)==find(b)) {
-					System.out.println("YES");
-				} else {
-					System.out.println("NO");
-				}
-			}
-		}
-	}
 
-	private static boolean union(int a, int b) {
-		int rootA = find(a);
-		int rootB = find(b);
-		if (rootA==rootB) return false;
-		
-		if (rank[rootA] < rank[rootB]) {
-			rank[rootB] += rank[rootA];
-			parents[rootA] = parents[rootB];
-		} else {
-			rank[rootA] += rank[rootB];
-			parents[rootB] = parents[rootA];
-		}
-		return true;
-	}
+    static int n, m;
+    static int[] parents;
+    static int[] size;
 
-	private static int find(int a) {
-		if (a==parents[a]) return parents[a];
-		return parents[a] = find(parents[a]);
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        parents = new int[n+1];
+        size = new int[n+1];
 
-	private static void makeSet() {
-		for (int i=0; i<=n; i++) {
-			parents[i] = i;
-			rank[i] = 1;
-		}
-	}
+        makeSet();
+
+        for (int i=0; i<m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int command = Integer.parseInt(st.nextToken());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+
+            if (command==0) {  // 합집합 연산
+                union(a,b);
+            } else {  // 같은 집합인지 확인하는 연산
+                if (findSet(a)==findSet(b)) System.out.println("YES");
+                else System.out.println("NO");
+            }
+        }
+    }
+
+    private static void makeSet() {
+        for (int i=1; i<=n; i++) {
+            parents[i] = i;
+            size[i] = 1;
+        }
+    }
+
+    private static boolean union(int a, int b) {
+        int aRoot = findSet(a);
+        int bRoot = findSet(b);
+
+        if (aRoot==bRoot) return false;
+        if (size[aRoot] < size[bRoot]) {
+            size[bRoot] += size[aRoot];
+            parents[aRoot] = bRoot;
+        } else {
+            size[aRoot] += size[bRoot];
+            parents[bRoot] = aRoot;
+        }
+        return true;
+    }
+
+    private static int findSet(int x) {
+        if (parents[x]==x) return parents[x];
+        return parents[x] = findSet(parents[x]);
+    }
 }
