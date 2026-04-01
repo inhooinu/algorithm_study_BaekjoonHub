8e2dceede3bf9943cebf4a36fc6d9fdb8acffb5a
inhooinu <59385391+inhooinu@users.noreply.github.com>
2026-04-01 16:07:32 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 928 ms, Memory: 280060 KB -BaekjoonHub

--- 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.java
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.java
@@ -1,84 +1,69 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.PriorityQueue;
+import java.util.Collections;
+import java.util.List;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-	static class Edge implements Comparable<Edge>{
-		int v, w;
-		public Edge(int v, int w) {
-			this.v = v;
-			this.w = w;
-		}
-		@Override
-		public int compareTo(Edge o) {
-			return this.w - o.w;
-		}
-		
-		
-	}
-	static int N, M, K, X;
-	static ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-	static int[] dist;
 	
-	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	static ArrayList<Integer> ans = new ArrayList<>(); 
+	static int N, M, K, X;  // 도시의 개수, 도로의 개수, 거리 정보, 출발 도시의 번호
+	static ArrayList<Integer>[] graph;
+	static List<Integer> result;
+	
 	public static void main(String[] args) throws IOException {
-		init();
-		dji();
-		for(int i=1; i<=N; i++) {
-			if(dist[i] == K) {
-				ans.add(i);
-			}
-		}
-		if(ans.isEmpty()) {
-			System.out.println(-1);
-		}else {
-			for(int n: ans) {
-				System.out.println(n);
-			}
-		}
-	}
-	
-	static void init() throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 		K = Integer.parseInt(st.nextToken());
 		X = Integer.parseInt(st.nextToken());
-		dist = new int[N+1];
-		Arrays.fill(dist, Integer.MAX_VALUE);
-		dist[X] = 0;
-		for(int i = 0; i<=N; i++) {
-			adj.add(new ArrayList<>());
+		graph = new ArrayList[N+1];
+		for (int i=1; i<=N; i++) {
+			graph[i] = new ArrayList<>();
 		}
-		
-		for(int i =0; i<M; i++) {
+		for (int i=0; i<M; i++) {
 			st = new StringTokenizer(br.readLine());
-			int v = Integer.parseInt(st.nextToken());
-			int w = Integer.parseInt(st.nextToken());
-			adj.get(v).add(w);
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			graph[a].add(b);
 		}
-	}
-	
-	static void dji() {
-		PriorityQueue<Edge> heap = new PriorityQueue<>();
-		heap.offer(new Edge(X, 0));
+		result = new ArrayList<>();
 		
-		while(!heap.isEmpty()) {
-			Edge cur = heap.poll();
-			if(dist[cur.v] < cur.w) continue;
-			for(int next: adj.get(cur.v)) {
-				if(dist[next] <= dist[cur.v] + 1) continue;
-				dist[next] = dist[cur.v] + 1;
-				heap.offer(new Edge(next, dist[next]));
+		bfs();
+		if (result.isEmpty()) {
+			System.out.println(-1);
+		} else {
+			Collections.sort(result);
+			for (int i=0; i<result.size(); i++) {
+				System.out.println(result.get(i));
 			}
 		}
 	}
-	
 
-
+	private static void bfs() {
+		Queue<int[]> q = new ArrayDeque<>();
+		boolean[] visited = new boolean[N+1];
+		
+		visited[X] = true;
+		q.offer(new int[] {X, 0});  // 출발도시
+		
+		while (!q.isEmpty()) {
+			int[] cur = q.poll();
+			int dist = cur[1];
+			if (dist==K) {
+				result.add(cur[0]);
+				continue;
+			}
+			
+			for (int next: graph[cur[0]]) {
+				if (visited[next]) continue;
+				visited[next] = true;
+				q.offer(new int[] {next, dist+1});
+			}
+		}
+	}
 }
