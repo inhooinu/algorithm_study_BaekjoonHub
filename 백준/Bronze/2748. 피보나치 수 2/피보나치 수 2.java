26c7047c2bde026d21639cdd9bd59a8f68b32357
inhooinu <59385391+inhooinu@users.noreply.github.com>
2026-04-08 09:12:42 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 96 ms, Memory: 13000 KB -BaekjoonHub

---
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+	
+	static int n;
+	static long[] fibo;
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		n = sc.nextInt();
+		fibo = new long[n+1];
+		
+		for (int i=0; i<=n; i++) {
+			if (i==0) fibo[i] = 0;
+			else if (i==1) fibo[i] = 1;
+			else fibo[i] = fibo[i-1] + fibo[i-2];
+		}
+		System.out.println(fibo[n]);
+	}
+}
