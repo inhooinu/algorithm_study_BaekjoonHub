bd10cf5c6fb4c797fd5558af7d49e09c77319f5b
inhooinu <59385391+inhooinu@users.noreply.github.com>
2026-05-03 16:10:10 +0900

    [level 2] Title: 선인장 숨기기, Time: 167.09 ms, Memory: 201 MB -BaekjoonHub

--- 프로그래머스/2/468379. 선인장 숨기기/선인장 숨기기.java
+++ 프로그래머스/2/468379. 선인장 숨기기/선인장 숨기기.java
@@ -17,69 +17,33 @@ class Solution {
         // for (int i=0; i<firstDropTime.length; i++) {
         //     System.out.println(Arrays.toString(firstDropTime[i]));
         // }
-        // System.out.println();
         
-        // 1. 각 행마다 w구간 최솟값 구하기
-        int[][] rowMin = new int[m][n-w+1];
-        for (int r=0; r<m; r++) {
-            Deque<Integer> dq = new ArrayDeque<>();
-            
-            for (int c=0; c<n; c++) {
-                while (!dq.isEmpty() && firstDropTime[r][dq.peekLast()] >= firstDropTime[r][c]) {
-                    dq.pollLast();
-                }
-                
-                dq.offerLast(c);
-                
-                if (dq.peekFirst() <= c-w) {  // w구간을 벗어난 경우 poll
-                    dq.pollFirst();
-                }
-                
-                if (c >= w-1) {
-                    rowMin[r][c-w+1] = firstDropTime[r][dq.peekFirst()];
-                }
-            }
-        }
-        // for (int r=0; r<m; r++) {
-        //     System.out.println(Arrays.toString(rowMin[r]));
-        // }
-        // System.out.println();
-        
-        // 2. 각 열마다 h구간 최솟값 구하기
         int maxTime = 0;
         int[] answer = new int[2];
-        for (int c=0; c<n-w+1; c++) {
-            Deque<Integer> dq = new ArrayDeque<>();
-            
-            for (int r=0; r<m; r++) {
-                while (!dq.isEmpty() && rowMin[dq.peekLast()][c] >= rowMin[r][c]) {
-                    dq.pollLast();
+        for (int i=0; i<m-h+1; i++) {
+            for (int j=0; j<n-w+1; j++) {
+                int time = checkTime(i,j,h,w, firstDropTime);
+                if (time>maxTime) {
+                    // System.out.println(time);
+                    maxTime = time;
+                    answer[0] = i;
+                    answer[1] = j;
                 }
-                
-                dq.offerLast(r);
-                
-                if (dq.peekFirst() <= r-h) {
-                    dq.pollFirst();
-                }
-                
-                if (r >= h-1) {
-                    int startR = r-h+1;
-                    int startC = c;
-                    
-                    int time = rowMin[dq.peekFirst()][c];
-                    
-                    if (time > maxTime) {
-                        maxTime = time;
-                        answer[0] = startR;
-                        answer[1] = startC;
-                    } else if (time==maxTime && answer[0] > startR) {
-                        answer[0] = startR;
-                        answer[1] = startC;
-                    }
-                }
+                if (time==drops.length+1) return answer;
             }
         }
         
         return answer;
+    }
+    
+    public int checkTime(int r, int c, int h, int w, int[][] firstDropTime) {
+        int time = Integer.MAX_VALUE;
+        for (int i=r; i<r+h; i++) {
+            for (int j=c; j<c+w; j++) {
+                time = Math.min(time, firstDropTime[i][j]);
+            }
+        }
+        
+        return time;
     }
 }
