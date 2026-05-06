b669b034085524e375286ec3642df54776a17026
inhooinu <59385391+inhooinu@users.noreply.github.com>
2026-05-06 16:59:12 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 79.29 ms, Memory: 97.4 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -2,23 +2,24 @@ import java.util.*;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        
-        Map<String, Integer> map = new HashMap<>();
-        
-        for (String name: participant) {
-            map.put(name, map.getOrDefault(name, 0)+1);
+        HashMap<String, Integer> map = new HashMap<>();
+        for (int i=0; i<participant.length; i++) {
+            map.put(participant[i], map.getOrDefault(participant[i], 0)+1);
         }
+        // System.out.println(map);
         
-        for (String name: completion) {
-            map.put(name, map.get(name)-1);
-        }
-        
-        for (String name: map.keySet()) {
-            if (map.get(name) > 0) {
-                return name;
+        for (int i=0; i<completion.length; i++) {
+            if (map.get(completion[i])==1) {
+                map.remove(completion[i]);
+            } else {
+                map.put(completion[i], map.get(completion[i])-1);
             }
         }
-        
+        // System.out.println(map.keySet());
+        Set<String> set = map.keySet();
+        for (String s: set) {
+            return s;
+        }
         return "";
     }
 }
