b4a9b74907b04e9cecd4de40d938a40f7d8b4d13
inhooinu <59385391+inhooinu@users.noreply.github.com>
2026-05-12 16:50:13 +0900

    [level 1] Title: K번째수, Time: 0.06 ms, Memory: 63.7 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -1,20 +1,31 @@
-import java.io.*;
 import java.util.*;
 
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
         
-        int[] answer = new int[commands.length];
+        ArrayList<Integer> answerList = new ArrayList<>();
         
         for (int c=0; c<commands.length; c++) {
-            // System.out.println(Arrays.toString(commands[c]));
             int i = commands[c][0];
             int j = commands[c][1];
             int k = commands[c][2];
             
-            int[] temp = Arrays.copyOfRange(array, i-1, j);
-            Arrays.sort(temp);
-            answer[c] = temp[k-1];
+            // array의 i번째부터 j번째까지 자르기
+            ArrayList<Integer> list = new ArrayList<>();
+            for (int idx=i-1; idx<j; idx++) {
+                list.add(array[idx]);
+            }
+            
+            // 오름차순 정렬
+            Collections.sort(list);
+            // System.out.println(list);
+            
+            answerList.add(list.get(k-1));
+        }
+        
+        int[] answer = new int[answerList.size()];
+        for (int i=0; i<answer.length; i++) {
+            answer[i] = answerList.get(i);
         }
         
         return answer;
